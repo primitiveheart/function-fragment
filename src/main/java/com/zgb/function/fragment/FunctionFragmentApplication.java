723b4c31c9424202879ebff131b89a9359c2941e
guanbao.zhou <guanabo.zhou@ximalaya.com>
2020-09-06 11:10:04 +0800

    版本工具

---
+++ src/main/java/com/zgb/function/fragment/FunctionFragmentApplication.java
@@ -0,0 +1,13 @@
+package com.zgb.function.fragment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionFragmentApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FunctionFragmentApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/zgb/function/fragment/FunctionFragmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zgb.function.fragment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionFragmentApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
